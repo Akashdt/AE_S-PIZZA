094bbe6913d0db41e860222d17e469f1fcd5d2e5
Akash <akashdt88@gmail.com>
2022-06-05 01:16:32 +0530

    commit

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ if(process.env.NODE_ENV ==='production')
 
 
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server is running on Port ${port}`);
